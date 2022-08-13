db65d571354a1a09ab6b279cbd13fd5c111b321b
zerous <zerous2070@163.com>
2022-08-13 10:55:08 +0800

    feat: add App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <router-view />
+    </div>
+</template>
+
+<style scoped>
+#app {
+    color: #2c3e50;
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    transform: rotate(0);
+}
+</style>
